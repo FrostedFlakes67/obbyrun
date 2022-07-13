7f1135fd124c124e659c3782ccea5199a6de7465
FrostedFlakes67 <66740113+FrostedFlakes67@users.noreply.github.com>
2022-07-12 22:59:10 -0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -23,4 +23,4 @@ while done==false do
     end
 end
 game:GetService("TeleportService"):Teleport(5434921173)
-syn.queue_on_teleport("<script to execute after TP>")
+syn.queue_on_teleport("loadstring(game:GetHttp('https://raw.githubusercontent.com/FrostedFlakes67/obbyrun/main/load.lua',true))()")
