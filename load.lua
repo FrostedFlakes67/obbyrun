9014c44f294b85d5416eead9361de8008e941493
FrostedFlakes67 <66740113+FrostedFlakes67@users.noreply.github.com>
2022-07-12 22:58:19 -0400

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,26 @@
+local lowest = 999999
+local done = false
+for _,v in pairs(game.Workspace.CurrentMap:GetChildren()) do
+    if string.sub(v.Name,1,11) == "Checkpoint_" then
+        if tonumber(string.sub(v.Name,12,#v.Name)) < lowest then
+            lowest = tonumber(string.sub(v.Name,12,#v.Name))
+        end
+    end
+end
+local lastdone = lowest-1
+while done==false do
+    if game.Workspace.CurrentMap:FindFirstChild("Checkpoint_"..lastdone+1) then
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game.Workspace.CurrentMap:FindFirstChild("Checkpoint_"..lastdone+1).Checkpoint,0)
+        wait()
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game.Workspace.CurrentMap:FindFirstChild("Checkpoint_"..lastdone+1).Checkpoint,1)
+        lastdone = lastdone+1
+        wait(2.5)
+    else
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game.Workspace.CurrentMap:FindFirstChild("Goal").Goal,0)
+        wait()
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game.Workspace.CurrentMap:FindFirstChild("Goal").Goal,0)
+        done = true
+    end
+end
+game:GetService("TeleportService"):Teleport(5434921173)
+syn.queue_on_teleport("<script to execute after TP>")
